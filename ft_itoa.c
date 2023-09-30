95ca722875a3290bff4e46c954bd8ec1cbeabf04
Cleo Le Tron <ale-tron@cbr7s1.42barcelona.com>
2023-09-30 10:09:09 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,11 +6,10 @@
 /*   By: ale-tron <ale-tron@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 13:27:00 by ale-tron          #+#    #+#             */
-/*   Updated: 2023/09/29 16:22:29 by ale-tron         ###   ########.fr       */
+/*   Updated: 2023/09/30 10:04:12 by ale-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <stdio.h>
 
 size_t	count_size(int n)
 {
@@ -29,6 +28,23 @@ size_t	count_size(int n)
 	return (count);
 }
 
+char	*zero_case(char *string)
+{
+	string[0] = 0 + '0';
+	return (string);
+}
+
+char	*fill_str(char *string, int n, size_t i)
+{
+	while (i > 0)
+	{
+		i--;
+		string[i] = n % 10 + '0';
+		n /= 10;
+	}
+	return (string);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*string;
@@ -41,28 +57,18 @@ char	*ft_itoa(int n)
 		return (NULL);
 	string[size] = '\0';
 	if (n == 0)
-		string[0] = 0 + '0';
+		return (zero_case(string));
+	i = size;
 	if (n == -2147483648)
 	{
-		string[0] = '-';
 		string[size - 1] = '8';
 		n = 214748364;
+		i = size - 1;
 	}
 	if (n < 0)
-	{
-		string[0] = '-';
 		n = -n;
-	}
-	i = size;
-	if (n == 214748364)
-		i = size - 1;
-	while (i > 0)
-	{
-		i--;
-		if (i == 0 && string[0] == '-')
-			break ;
-		string[i] = n % 10 + '0';
-		n /= 10;
-	}
+	fill_str(string, n, i);
+	if (string[0] == '0')
+		string[0] = '-';
 	return (string);
 }
